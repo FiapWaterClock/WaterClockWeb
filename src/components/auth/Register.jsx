71c1a1d376ff1f801c7a2dbff9804eab5a5e9deb
Kamyla Aragão <kamyla.aragao55@gmail.com>
2019-10-06 12:08:57 -0300

    Add fields to register form and show error messages

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,17 +1,18 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import Input from '../shared/Input'
-import { connect } from 'react-redux'
-import { registerAction, redirect } from '../../core/store/actions/authActions'
+import {connect} from 'react-redux'
+import {registerAction, redirect} from '../../core/store/actions/authActions'
 
 class Register extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            name: '',
+            firstName: '',
+            lastName: '',
             email: '',
             password: '',
-            repeat: ''
+            matchingPassword: ''
         }
 
         this.onChangeHandler = this.onChangeHandler.bind(this)
@@ -25,17 +26,23 @@ class Register extends Component {
     onSubmitHandler(e) {
         e.preventDefault()
 
-        this.props.register(this.state.name, this.state.email, this.state.password)
+        this.props.register(this.state.firstName, this.state.lastName, this.state.email, this.state.password, this.state.matchingPassword)
     }
 
     componentWillReceiveProps(newProps) {
         if (newProps.registerSuccess) {
-            this.props.redirect()
-            this.props.history.push('/login')
+            this.props.redirect("/login");
+            this.props.history.push('/login');
         }
     }
 
     render() {
+        const registerError = this.props.registerError;
+        let alert = "";
+        if(registerError) {
+            alert = <div className="alert alert-danger" role="alert">{registerError.error_description}</div>
+        }
+
         return (
             <div>
                 <div className="col-sm-12">
@@ -47,10 +54,18 @@ class Register extends Component {
                 <form onSubmit={this.onSubmitHandler}>
                     <div className="form-group">
                         <Input
-                            name="name"
-                            value={this.state.name}
+                            name="firstName"
+                            value={this.state.firstName}
                             onChange={this.onChangeHandler}
-                            label="Name"
+                            label="First Name"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <Input
+                            name="lastName"
+                            value={this.state.lastName}
+                            onChange={this.onChangeHandler}
+                            label="Last Name"
                         />
                     </div>
                     <div className="form-group">
@@ -72,9 +87,9 @@ class Register extends Component {
                     </div>
                     <div className="form-group">
                         <Input
-                            name="repeat"
+                            name="matchingPassword"
                             type="password"
-                            value={this.state.repeat}
+                            value={this.state.matchingPassword}
                             onChange={this.onChangeHandler}
                             label="Repeat password"
                         />
@@ -88,6 +103,9 @@ class Register extends Component {
                         </div>
                     </div>
                 </form>
+
+                {alert}
+
             </div>
         )
     }
@@ -95,13 +113,14 @@ class Register extends Component {
 
 function mapState(state) {
     return {
-        registerSuccess: state.register.success
+        registerSuccess: state.register.success,
+        registerError: state.register.error
     }
 }
 
 function mapDispatch(dispatch) {
     return {
-        register: (name, email, password) => dispatch(registerAction(name, email, password)),
+        register: (firstName, lastName, email, password, matchingPassword) => dispatch(registerAction(firstName, lastName, email, password, matchingPassword)),
         redirect: () => dispatch(redirect())
     }
 }
